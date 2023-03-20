4eb369f5270134dad8d42fea8bd2ef09df582e75
joel <joelotic@gmail.com>
2023-03-20 18:27:14 +0800

    office

--- structural-directives/src/app/test/test.component.ts
+++ structural-directives/src/app/test/test.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `
+
+    <h3>
+      ******* ngIf  *******
+    </h3>
+    <div *ngIf="isTrue;then thenBlockj; else elseBlockj">
+      Joel Code
+    </div>
+
+    <ng-template #thenBlockj>
+    <h2>
+      Name is Showing here
+    </h2>
+    </ng-template>
+    <ng-template #elseBlockj>
+      <h2>
+      Name is Hidden
+      </h2>
+    </ng-template>
+
+    <h3>
+      ******* ngSwitch  *******
+    </h3>
+    <div  [ngSwitch]="color" >
+      <div *ngSwitchCase="'red'"> You picked red color </div>
+      <div *ngSwitchCase="'green'"> You picked green color </div>
+      <div *ngSwitchCase="'blue'">You picked blue color</div>
+      <div *ngSwitchDefault> Pick a color</div>
+    </div>
+  `,
   styleUrls: ['./test.component.css']
 })
-export class TestComponent {
+export class TestComponent implements OnInit {
 
+  public isTrue = false
+  public color = "bluex"
+  constructor(){}
+  ngOnInit(){}
 }
