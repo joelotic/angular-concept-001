bee3e74ceb0a6f592ca59e4520fdcebf233397dc
joel <joelotic@gmail.com>
2023-03-20 17:43:29 +0800

    office1

--- event-binding/src/app/test/test.component.ts
+++ event-binding/src/app/test/test.component.ts
@@ -5,19 +5,20 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <h2>Welcome : {{name}}</h2>
     <button (click)="onClick($event)" >Greet</button>
-    <button (click)="greeting='Welcome Joel'" >Greet Click</button>
+    <button (click)="greeting='Welcome Joel to event binding'"  >Greet Click</button>
     {{greeting}}
 
   `,
   styleUrls: ['./test.component.css']
 })
+// (click)="greeting='Welcome Joel'"
 export class TestComponent implements OnInit {
 
   public name ="Joel Code"
   public greeting = ""
   constructor(){}
   ngOnInit(): void {
-    
+
   }
   onClick(event: MouseEvent){
     console.log(event)
--- template-reference-var/src/app/test/test.component.ts
+++ template-reference-var/src/app/test/test.component.ts
@@ -4,22 +4,24 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-test',
   template: `
     <h2>Welcome : {{name}}</h2>
-    <input #myInput  type="text"> 
+    <input #myInput  type="text">
     <button (click)="logMessage(myInput.value)"> Log </button>
-    
+    <h2> button click inpute is : {{input}} </h2>
   `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
   public name = "Joel Code"
+  public input =""
   constructor(){}
   ngOnInit(): void {
-    
+
   }
 
   logMessage(value: any){
     console.log(value)
+    this.input = value
   }
 
 }
